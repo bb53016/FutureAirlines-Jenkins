ba647aa74c480ebb4caaeaecafb604b52dc28a41
bb53016 <banambab@gmail.com>
2019-12-23 05:05:54 -0500

    Update pipeline.groovy

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -2,9 +2,6 @@
 
 node ('deployer') {
 
-  writeFile(file: "git-askpass-${BUILD_TAG}", text: "#!/bin/bash\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
-  sh "chmod a+x git-askpass-${BUILD_TAG}"
-
   dir(env) {
 
     stage ('GitHub Checkout') {
