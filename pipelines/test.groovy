f2d93842e15e82f218a578f599c217861354f2c3
bb53016 <banambab@gmail.com>
2019-12-23 05:58:56 -0500

    Update test.groovy

--- pipelines/test.groovy
+++ pipelines/test.groovy
@@ -1,22 +1,17 @@
 
 
 node ('deployer') {
+  writeFile(file: "git-askpass-${BUILD_TAG}", text: "#!/bin/bash\ncase \"\$1\" in\nUsername*) echo \"\${STASH_USERNAME}\" ;;\nPassword*) echo \"\${STASH_PASSWORD}\" ;;\nesac")
+  sh "chmod a+x git-askpass-${BUILD_TAG}"
 
   dir(env) {
 
     stage ('GitHub Checkout') {
       git_checkout()
-       }
+       }   
 	  
-    stage('Test Code') {
-        steps {
-           echo 'Testing Code...'
-            }
-        }    
-	    
    //Build EC2 instance using terraform to test code
-
-    dir(terraformdir_compute) {
+  stage('start test ec2 instance') {
 
        stage ('Compute Stack Remote State') {
          terraformKey = "app_compute.tfstate"
@@ -33,7 +28,12 @@ node ('deployer') {
            terraform_apply()
          }
        }
-
+	  
+    stage('Test Code') {
+        steps {
+           echo 'Testing Code...'
+            }
+        }
      }
 
   }
